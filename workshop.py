2cddaed654d5220d37d0fa2296728286f8f58b88
Thiskk040 <Khunanon.kamin@gmail.com>
2026-07-14 16:16:51 +0700

    save1

---
+++ workshop.py
@@ -0,0 +1,10 @@
+quantity = 100 
+cost_price = 2000
+sell_price = 3000
+team_members = 50
+
+
+ต้นทุนทั้งหมด = quantity * cost_price
+รายรับ = quantity * sell_price
+กำไร = sell_price - รายรับ
+จำนวนเงินที่ถูกหักไปให้บอส = กำไร * 0.2
